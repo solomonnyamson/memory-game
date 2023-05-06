392373e1a671bdb977a8c2fddbdf618e32818091
solomonnyamson <solomon.nyamson@amalitech.org>
2023-05-06 17:26:55 +0000

    feat: Implement start game functionality to select the correct game board - Mobile View

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
-const startButton = document.querySelector('.menu-button');
-const gameBoardWrapper = document.querySelector('.wrapper');
+const startButton = document.querySelector(".menu-button");
+const selectedTheme = document.getElementsByName("theme");
+const selectedPayers = document.getElementsByName("player");
+const selectedSize = document.getElementsByName("size");
+const initialGameScreen = document.querySelector(".wrapper");
+const gameStartScreen = document.querySelector(".game-start");
+const playGameScreen4x4 = document.querySelector(".c4x4");
+const playGameScreen6x6 = document.querySelector(".c6x6");
+const soloGameBottom = document.querySelector(".bottom-timer-moves");
+const multiGameBottom = document.querySelector(".bottom-player-moves");
 
+const allOptions = [...selectedTheme, ...selectedPayers, ...selectedSize];
 
-startButton.addEventListener('click', () => {
-    gameBoardWrapper.classList.add('game-start')
-})
+const selectedOptions = {};
+
+startButton.addEventListener("click", () => {
+  getSelectedOptions();
+  selectGameBoard(selectedOptions);
+});
+
+// USER DEFINED  FUNCTIONS
+const getSelectedOptions = () => {
+  for (let option in allOptions) {
+    if (allOptions[option].checked) {
+      selectedOptions[allOptions[option].name] = allOptions[option].value;
+    }
+  }
+};
+
+const selectGameBoard = (options) => {
+  // TODO: ADD ADDITIONAL BLOCKS TO CHECK FOR ALL OPTIONS
+
+  initialGameScreen.classList.add("hide");
+  gameStartScreen.classList.remove("hide");
+  if (
+    options?.theme == "numbers" &&
+    options?.player == "2" &&
+    options?.size == "4x4"
+  ) {
+    soloGameBottom.classList.add("hide");
+    multiGameBottom.classList.remove("hide");
+  } else if (
+    options?.theme == "numbers" &&
+    options?.player == "1" &&
+    options?.size == "6x6"
+  ) {
+    playGameScreen4x4.classList.add("hide");
+    playGameScreen6x6.classList.remove("hide");
+  } else if (
+    options?.theme == "numbers" &&
+    options?.player == "1" &&
+    options?.size == "6x6"
+  ) {
+  }
+};
